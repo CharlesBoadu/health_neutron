fe6f8087f516064edc410daf125fb9e598021fda
CharlesBoaduAmalitech <charles.boadu@amalitech.org>
2023-05-06 23:47:24 +0000

    Adding changes

--- components/LabSmallCards.jsx
+++ components/LabSmallCards.jsx
@@ -10,51 +10,51 @@ function LabSmallCards() {
 
   return (
     <>
-      <div className="flex flex-row justify-center items-center md:space-x-3 space-x-10 py-2 z-1 hover:cursor-pointer" onClick={handleNavigationToLabs}>
+      <div className="flex flex-row justify-center items-center md:space-x-3 space-x-7 py-2 z-1 hover:cursor-pointer" onClick={handleNavigationToLabs}>
         <div className="flex flex-col">
-          <div className="md:w-[150px] md:h-[100px] w-[380px] h-[250px] bg-gray-300">
+          <div className="md:w-[150px] md:h-[100px] w-[100px] bg-gray-300">
             <img
               src="/HN-MOM&DAD(STA).jpg"
               alt="Lab Image 1"
               className="object-cover h-full w-full"
             />
           </div>
-          <div className="md:text-sm text-3xl text-gray-600 flex flex-col font-montserrat">
+          <div className="md:text-sm text-xs text-gray-600 flex flex-col font-montserrat">
             <div>Honor your Mom</div>
             <div>{"&"} Dad (Standard)</div>
           </div>
         </div>
         <div className="flex flex-col">
-          <div className="md:w-[150px] md:h-[100px] w-[380px] h-[250px] bg-gray-300">
+          <div className="md:w-[150px] md:h-[100px] w-[100px] bg-gray-300">
             <img
               src="/PCR -Covid19.png"
               alt="Lab Image 2"
               className="object-cover h-full w-full"
             />
           </div>
-          <div className="md:text-sm text-3xl text-gray-600 font-montserrat">Covid - Express</div>
+          <div className="md:text-sm text-xs text-gray-600 font-montserrat">Covid - Express</div>
         </div>
       </div>
-      <div className="flex flex-row justify-center items-center md:space-x-3 space-x-10 py-2 hover:cursor-pointer" onClick={handleNavigationToLabs}>
+      <div className="flex flex-row justify-center items-center md:space-x-3 space-x-7 py-2 hover:cursor-pointer" onClick={handleNavigationToLabs}>
         <div className="flex flex-col">
-          <div className="md:w-[150px] md:h-[100px] w-[380px] h-[250px] bg-gray-300">
+          <div className="md:w-[150px] md:h-[100px] w-[100px] bg-gray-300">
             <img
               src="/Covid - Standard.png"
               alt="Lab Image 3"
               className="object-cover h-full w-full"
             />
           </div>
-          <div className="md:text-sm text-3xl text-gray-600 font-montserrat">Covid - Standard</div>
+          <div className="md:text-sm text-xs text-gray-600 font-montserrat">Covid - Standard</div>
         </div>
         <div className="flex flex-col">
-          <div className="md:w-[150px] md:h-[100px] w-[380px] h-[250px] bg-gray-300">
+          <div className="md:w-[150px] md:h-[100px] w-[100px] bg-gray-300">
             <img
               src="/HN-MOM&DAD{PREM.).jpg"
               alt="Lab Image 4"
               className="object-cover h-full w-full"
             />
           </div>
-          <div className="md:text-sm text-3xl text-gray-600 font-montserrat">
+          <div className="md:text-sm text-xs text-gray-600 font-montserrat">
             <div>Honor your Mom</div>
             <div>{"&"} Dad (Premium)</div>
           </div>
--- components/VaccinesSmallCards.jsx
+++ components/VaccinesSmallCards.jsx
@@ -12,30 +12,30 @@ function VaccinesSmallCards() {
     <>
       <div className="flex flex-row justify-center items-center md:space-x-3 space-x-10 py-2 z-1 hover:cursor-pointer" onClick={handleNavigationToVaccines}>
         <div className="flex flex-col">
-          <div className="md:w-[150px] md:h-[100px] w-[380px] h-[250px] bg-gray-300">
+          <div className="md:w-[150px] md:h-[100px] w-[100px] bg-gray-300">
             <img src="/HBV.jpg" alt="Lab Image 1" className="object-cover h-full w-full"/>
           </div>
-          <div className="md:text-sm text-3xl text-gray-600 font-montserrat">HBV Vaccine</div>
+          <div className="md:text-sm text-xs text-gray-600 font-montserrat">HBV Vaccine</div>
         </div>
         <div className="flex flex-col">
-          <div className="md:w-[150px] md:h-[100px] w-[380px] h-[250px] bg-gray-300">
+          <div className="md:w-[150px] md:h-[100px] w-[100px] bg-gray-300">
             <img src="/vaccine_services.jpg" alt="Lab Image 2" className="object-cover h-full w-full"/>
           </div>
-          <div className="md:text-sm text-3xl text-gray-600 font-montserrat">Vaccine Service</div>
+          <div className="md:text-sm text-xs text-gray-600 font-montserrat">Vaccine Service</div>
         </div>
       </div>
       <div className="flex flex-row justify-center items-center md:space-x-3 space-x-10 py-2 hover:cursor-pointer" onClick={handleNavigationToVaccines}>
         <div className="flex flex-col">
-          <div className="md:w-[150px] md:h-[100px] w-[380px] h-[250px] bg-gray-300">
+          <div className="md:w-[150px] md:h-[100px] w-[100px] bg-gray-300">
             <img src="/vaccine_services.jpg" alt="Lab Image 3" className="object-cover h-full w-full"/>
           </div>
-          <div className="md:text-sm text-3xl text-gray-600 font-montserrat">Vaccine 3</div>
+          <div className="md:text-sm text-xs text-gray-600 font-montserrat">Vaccine 3</div>
         </div>
         <div className="flex flex-col">
-          <div className="md:w-[150px] md:h-[100px] w-[380px] h-[250px] bg-gray-300">
+          <div className="md:w-[150px] md:h-[100px] w-[100px] bg-gray-300">
             <img src="/vaccine_services.jpg" alt="Lab Image 4" className="object-cover h-full w-full"/>
           </div>
-          <div className="md:text-sm text-3xl text-gray-600 font-montserrat">Vaccine 4</div>
+          <div className="md:text-sm text-xs text-gray-600 font-montserrat">Vaccine 4</div>
         </div>
       </div>
     </>
